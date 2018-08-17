3d94dc5d62abb3e2fd3ebcfe373028657ae8d56e
Dustin Brown <dbrown@smoothfusion.com>
2018-08-16 21:32:08 -0500

    oops

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    email: "dustin.w.brown@gmail.com",
+    linkedin: "https://www.linkedin.com/in/dustinbrown/",
+    twitter: "https://twitter.com/WillCode2Surf",
+    github: "https://github.com/willCode2Surf",
+    dev: {
+      to: "oops"
+    }
+  };
